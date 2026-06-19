9aded133714147c33b122095320e109c882b8fe3
prettysorrow <prettysorrow44@gmail.com>
2026-06-19 05:08:22 +0300

    add backend db context
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

---
+++ Backend/BackDbContext.cs
@@ -0,0 +1,30 @@
+namespace Backend;
+
+using Microsoft.EntityFrameworkCore;
+
+public class BackDbContext : DbContext
+{
+    public BackDbContext(DbContextOptions<BackDbContext> options) : base(options) { }
+    public DbSet<DbModels.VerboseUser> VerboseUsers => Set<DbModels.VerboseUser>();
+    public DbSet<DbModels.FollowerUser> Followers => Set<DbModels.FollowerUser>();
+    public DbSet<DbModels.FollowingUser> Followings => Set<DbModels.FollowingUser>();
+    public DbSet<DbModels.Request> Requests => Set<DbModels.Request>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbModels.FollowerUser>().HasKey(e => e.UID);
+        modelBuilder.Entity<DbModels.FollowingUser>().HasKey(e => e.UID);
+        modelBuilder.Entity<DbModels.Request>().HasKey(e => e.UID);
+        modelBuilder.Entity<DbModels.VerboseUser>().HasKey(e => e.UID);
+
+        modelBuilder.Entity<DbModels.VerboseUser>()
+            .HasMany(e => e.UserFollowers)
+            .WithOne()
+            .HasForeignKey("FollowerOfUID");
+
+        modelBuilder.Entity<DbModels.VerboseUser>()
+            .HasMany(e => e.UserFollowing)
+            .WithOne()
+            .HasForeignKey("FollowingOfUID");
+    }
+}
