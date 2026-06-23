4f143cedf6b4985effc181d0f61e3b128be14ad6
prettysorrow <prettysorrow44@gmail.com>
2026-06-24 00:59:13 +0300

    improve script for backend database management
    Signed-off-by: prettysorrow <prettysorrow44@gmail.com>

--- Scripts/backend-db.lua
+++ Scripts/backend-db.lua
@@ -1,61 +1,129 @@
-local CONTAINER = "web-hound-backend-db"
-local SERVICE   = "backend-db"
+local CONTAINER   = "web-hound-backend-db"
+local SERVICE     = "backend-db"
 
-local function exec(cmd)
-    local ok = os.execute(cmd)
-    return ok == true or ok == 0
+local log_tag     = {
+    Fatal = "[fatal]",
+    Info = "[info]",
+    Warning = "[warning]"
+}
+
+local log_channel = {
+    Fatal = io.stderr,
+    Info = io.stdout,
+    Warning = io.stderr
+}
+
+local function write_log(tag, msg)
+    if (msg == nil or msg == "") then
+        io.stderr:write("Warning" .. " logger was called with empty message" .. "\n")
+        return
+    end
+
+    if (tag == nil or tag == "") then
+        io.stderr:write("Warning" .. " logger was called with empty tag" .. "\n")
+        return
+    end
+
+    log_channel[tag]:write(log_tag[tag] .. " " .. msg .. "\n")
+end
+
+local function make_failfast(msg)
+    if (msg == nil or msg == "") then
+        return function()
+            os.exit(1)
+        end
+    end
+
+    return function()
+        write_log("Fatal", msg)
+        os.exit(1)
+    end
+end
+
+local function exec(cmd, on_err)
+    local rez = os.execute(cmd)
+    if (rez ~= 0 and rez ~= true)
+    then
+        if (on_err ~= nil) then
+            on_err()
+        else
+            write_log("Warning",
+                'something went wrong while executing "' .. cmd .. '" but error handler was not specified')
+        end
+    end
 end
 
 local function container_exists()
-    return exec('docker ps -a | grep "' .. CONTAINER .. '" > /dev/null 2>&1')
+    local code = os.execute('docker container inspect "' .. CONTAINER .. '" > /dev/null 2>&1')
+    return code == true or code == 0
+end
+
+local function fresh_migration_name()
+    return ('postgre_' .. os.date("%Y_%m_%d_%H_%M_%S"))
+end
+
+local function add_migration()
+    local migration_name = fresh_migration_name()
+    local on_err = make_failfast("can not add migration")
+    exec(
+        'dotnet ef migrations add'
+        .. ' "' .. migration_name .. '"'
+        .. ' --project EntityFramework '
+        .. ' --startup-project Backend',
+        on_err)
+    exec('dotnet ef database update '
+        .. ' --project EntityFramework '
+        .. '--startup-project Backend',
+        on_err)
 end
 
 local commands = {}
 
 commands["create"] = function()
     if container_exists() then
-        print("nothing to do")
+        write_log("Warning", "container_exists, nothing to do")
         return
     end
-    exec('docker compose up -d')
-    exec('dotnet ef migrations add FreshBackendDatabase --project Backend')
-    exec('dotnet ef database update                     --project Backend')
+
+    exec('docker compose up -d ' .. SERVICE, make_failfast("can not compose container"))
+    add_migration()
 end
 
 commands["update"] = function()
     if not container_exists() then
-        print("database was not created")
-        os.exit(1)
+        make_failfast("container does not exist")()
     end
-    exec('dotnet ef migrations add UpdateBackendDatabase --project Backend')
-    exec('dotnet ef database update                      --project Backend')
+
+    add_migration()
 end
 
 commands["drop"] = function()
     if not container_exists() then
-        print("nothing to do")
+        write_log("Warning", "container does not exist, nothing to do")
         return
     end
-    exec('docker compose stop "'   .. SERVICE .. '"')
-    exec('docker compose down -v "' .. SERVICE .. '"')
+
+    exec('docker compose down -v "' .. SERVICE .. '"', make_failfast("can not drop container"))
     exec('rm -rf Database')
-    exec('rm -rf Backend/Migrations')
+    exec('rm -rf EntityFramework/Migrations')
 end
 
 commands["stop"] = function()
-    if container_exists() then
-        exec('docker compose stop "' .. SERVICE .. '"')
-    else
-        print("nothing to do")
+    if not container_exists() then
+        write_log("Warning", "container does not exist, nothing to do")
+        return
     end
+
+    exec('docker compose stop "' .. SERVICE .. '"')
 end
 
 commands["start"] = function()
     if not container_exists() then
-        exec('docker compose up -d "' .. SERVICE .. '"')
+        write_log("Info", "container does not exist, so i am trying to create it")
+        exec('docker compose up -d "' .. SERVICE .. '"', make_failfast("can not create container"))
     end
 
-    exec('docker compose start "' .. SERVICE .. '"')
+    exec('docker compose start "' .. SERVICE .. '"', make_failfast("can not start container"))
 end
 
 local cmd = arg[1]
